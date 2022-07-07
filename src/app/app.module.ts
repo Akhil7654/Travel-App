588bc569d5a624b6afce3daf9cd05f2521ff8ede
unknown <msdcr88@gmail.com>
2022-07-08 00:46:02 +0530

    travel app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
+import { ViewdestinationsComponent } from './viewdestinations/viewdestinations.component';
+import { SearchplacesComponent } from './searchplaces/searchplaces.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    TravelRegistrationComponent,
+         ViewdestinationsComponent,
+         SearchplacesComponent
   ],
   imports: [
     BrowserModule,
